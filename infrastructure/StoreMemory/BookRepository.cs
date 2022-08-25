73c300c14ac84c953e9c8fb2792101724f7a5abd
Roman <bffus@mail.ru>
2022-08-25 19:58:34 +0400

    Реализовать поиск по ISBN и автору

--- infrastructure/StoreMemory/BookRepository.cs
+++ infrastructure/StoreMemory/BookRepository.cs
@@ -7,14 +7,26 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "Art Of Programming"),
-            new Book(2, "Refactoring"),
-            new Book(3, "C Programming Language"),
+            new Book(1, "ISBN 12312-32131", "D. Knuth", "Art Of Programming"),
+            new Book(2, "ISBN 12312-32132", "M. Knuth", "Refactoring"),
+            new Book(3, "ISBN 12312-32133", "B. Knuth", "C Programming Language"),
         };
+
+        public Book[] GetAllByIsbn(string isbn)
+        {
+            return books.Where(book => book.Isbn == isbn).ToArray();
+        }
 
         public Book[] GetAllByTitle(string titlePart)
         {
             return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string query)
+        {
+            return books.Where(book => book.Author.Contains(query)
+                                    || book.Title.Contains(query))
+                                    .ToArray();
         }
     }
 }
